65bc70e59610778a349006e745716a4e5c0e8780
zhp <zhp@zhpdeMBP.lan>
2016-08-18 07:41:51 +0800

    auto commit

--- autopush.sh
+++ autopush.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-git add *
-git commit -a -m "auto commit"
-git push
 
-date +"%F %T" > ./lastupdate
+
+x=$((RANDOM%200))
+if [ "$x" -gt 100 ];
+then
+
+	git add *
+	git commit -a -m "auto commit"
+	git push
+
+	date +"%F %T" > ./lastupdate
+
+fi
+
